fdd844768202cabd621e71d084941c4fe5dbc823
FERNANDO BARTOLI <fdbartoli91@gmail.com>
2023-08-07 12:08:26 -0300

    se agregan formularios reactivos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
+
+
 export class BasicPageComponent {
 
-}
+constructor (private fb : FormBuilder){}
+
+
+  public myForm: FormGroup = this.fb.group({
+    name: [''],
+    price:[0],
+    inStorage : [0]  
+  })
+      
+  onSave(): void{
+    console.log(this.myForm.value)
+  }
+
+  }
