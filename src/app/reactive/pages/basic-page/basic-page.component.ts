b3d9a918b86a8a363b40fccbf48b3c0d893f1ead
FERNANDO BARTOLI <fdbartoli91@gmail.com>
2023-08-07 15:52:00 -0300

    Se agrega reset de formulaio

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const rtx5900 = {
+  name: 'RTX 5090',
+  price: 2500,
+  inStorage: 6
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -8,9 +14,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 
 
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
 constructor (private fb : FormBuilder){}
+  ngOnInit(): void {
+    this.myForm.reset(rtx5900)
+  }
 
 
   public myForm: FormGroup = this.fb.group({
@@ -22,7 +31,10 @@ constructor (private fb : FormBuilder){}
   onSave(): void{
 
     if(this.myForm.invalid) return;
+
     console.log(this.myForm.value)
+
+    this.myForm.reset({price: 0, inStorage: 0});
   }
 
   }
